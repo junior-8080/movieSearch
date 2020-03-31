3f9e35dac443e63d17e1f740fcfa42617e76e3c7
abdul mukhsin <aabdulmukhsin@gmail.com>
2020-03-31 15:00:57 -0400

    testing Routing

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,60 @@
-import React  from  'react'
+import React, { Component }  from  'react'
 import Searchbar from './Search.js'
 import logo from "./logo4.jpeg"
+import MovieList from "./MovieList"
+let api_key = process.env.REACT_APP_API_KEY
 
-function Home(props){
-    
-    const divStyle={
-        width:"100%",
-        maxHeight:"auto"
+class  Home extends Component{
+    constructor(){
+        super()
+        this.state={
+            query:"",
+            movies:[]
+        }
     }
-    const spanStyle={
-        fontSize:"smaller"
+    handleChange=(event)=>{
+        
+        this.setState({
+            query:event.target.value
+        })
     }
-    return(
-        <div  className="home">
-            <div style={divStyle}>
-                 <img src={logo} s width="100%" height="80%" alt="Home-image"/>
+    handleSubmit = (event)=>{
+        event.preventDefault()
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${this.state.query}`)
+            .then(res=> res.json())
+            .then(data =>{
+                this.setState({
+                    movies : data.result
+                })
+            })
+    }
+    render(){
+        const divStyle={
+            width:"100%",
+            maxHeight:"auto"
+        }
+        const spanStyle={
+            fontSize:"smaller"
+        }
+        let movieList = this.state.movies.map((movie,i)=>{
+            return <MovieList  key={i}  movie={movie}/>
+        })
+        return(
+            <div  className="home">
+                <div style={divStyle}>
+                     <img src={logo} s width="100%" height="80%" alt="Home-image"/>
+                </div>
+                <div style={divStyle}>
+                     <Searchbar handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+                     <span style={spanStyle}>Search for your favorite movies</span>
+                     <div>
+                            {movieList}
+                     </div>
+                </div>
+                
             </div>
-            <div style={divStyle}>
-                 <Searchbar />
-                 <span style={spanStyle}>Search for your favorite movies</span>
-            </div>
-            
-        </div>
     )
+    }
 }
 
 export default Home
--- src/Search.js
+++ src/Search.js
@@ -2,13 +2,9 @@ import React from 'react'
 import './index.css'
 
 function Searchbar(props){
-    const style = {
-        width:"100%"
-    }
-
     return(
-        <form style={style} className="search" onSubmit={props.handleSubmit}>
-            <input type="text"  value={props.value} onChange={props.handleChange}/>
+        <form onSubmit={props.handleSubmit} className="search" >
+            <input type="text" onChange={props.handleChange} />
             <button type="submit" onClick={props.handleSubmit}>Search</button>
         </form>
     )
